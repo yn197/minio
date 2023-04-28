12b454e1936300e7ae10884ce83ce75ce4489e20
yn197 <1607401968@qq.com>
2023-04-28 22:58:44 +0800

    windows启动minio脚本文件

---
+++ start.bat
@@ -0,0 +1,5 @@
+set MINIO_ROOT_USER=admin
+
+set MINIO_ROOT_PASSWORD=admin123
+
+minio.exe server   --console-address "127.0.0.1:9000"  --address "127.0.0.1:9090"   F:\date1 F:\date2 F:\date3 F:\date4
